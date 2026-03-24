2cc26474ba1df2bffb7b5cab54d23211e6eb76ba
OttoLP <otoniel.lro@gmail.com>
2026-03-24 17:29:59 -0600

    Subir parciales y reorganizar estructura del curso

---
+++ Parcial 2/JS/script.js
@@ -0,0 +1,7 @@
+function saludar() {
+    const a = 5;
+    const b = 10;
+    const c = a + b;
+    console.log(c);
+    alert("INGA TUUUU " + c);
+}
---
+++ Parcial 2/MENU RESPONSIVO/main.js
@@ -0,0 +1,12 @@
+const menu = document.querySelector('nav');
+
+function toogleMenu() {
+    // if (menu.classList.contains('open')) {
+    //     menu.classList.remove('open');
+    // } else {
+    //     menu.classList.add('open');
+    // }
+
+    menu.classList.toggle('open');
+
+}
